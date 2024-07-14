03760cbcd4efcc7714d583e6ee041516bf875073
miso-taku <wk.ta93mis@gmail.com>
2024-07-14 15:27:57 +0900

    CRUD CR作成

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,23 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
 import api.schemas.task as task_schema
+import api.cruds.task as task_crud
+from api.db import get_db
 
 router = APIRouter()
 
 # タスクの一覧を取得する
 @router.get("/tasks", response_model=list[task_schema.Task])
-async def list_tasks():
-    return [task_schema.Task(id=1, title="1つ目のタスク")]
+async def list_tasks(db: Session = Depends(get_db)):
+    return task_crud.get_tasks_with_done(db)
 
 # タスクを作成する
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+async def create_task(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
+    return task_crud.create_task(db, task_body)
 
 # タスクを取得する
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
-async def update_task(task_id: int, task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
+async def update_task(task_id: int, task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
+    task = task_crud.get_task(db, task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+                      
+    return task_crud.update_task(db, task_body, original=task)
 
 # タスクを削除する
 @router.delete("tasks/{task_id}", response_model=None)
